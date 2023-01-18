56c42008f142f17a59b4b82f48cc44324509f2d5
Emirgazydev <emirgazyfirdavus@gmail.com>
2023-01-18 09:17:38 +0600

    travel

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
